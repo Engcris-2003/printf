d103716361fd30a9f68111ac84d4fd0b1c06ebcc
Odarlie <odarlie@gmail.com>
2022-07-11 12:57:04 +0300

    Adding additional lines to the printf function

--- _printf.c
+++ _printf.c
@@ -1,15 +1,9 @@
 #include "main.h"
-
 /**
- *
- *   * _printf - function that prints based on format specifier
- *
- *     * @format: takes in format specifier
- *
- *       * Return: return pointer to index
- *
- *         */
-
+  * _printf - function that prints based on format specifier
+  * @format: takes in format specifier
+  * Return: return pointer to index
+  */
 int _printf(const char *format, ...)
 {
 	char buffer[1024];
@@ -21,4 +15,35 @@ int _printf(const char *format, ...)
 		{'o', format_o}, {'b', format_b}, {'p', format_p}, {'r', format_r},
 		{'R', format_R}, {'\0', NULL}
 	};
-
+	if (!format)
+		return (-1);
+	va_start(valist, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		for (; format[i] != '%' && format[i] != '\0'; *index += 1, i++)
+		{
+			if (*index == 1024)
+			{	_write_buffer(buffer, index);
+				reset_buffer(buffer);
+				*index = 0;
+			}
+			buffer[*index] = format[i];
+		}
+		if (format[i] == '\0')
+			break;
+		if (format[i] == '%')
+		{	i++;
+			for (j = 0; spec[j].tp != '\0'; j++)
+			{
+				if (format[i] == spec[j].tp)
+				{	spec[j].f(valist, buffer, index);
+					break;
+				}
+			}
+		}
+	}
+	va_end(valist);
+	buffer[*index] = '\0';
+	_write_buffer(buffer, index);
+	return (*index);
+}
